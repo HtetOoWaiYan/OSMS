ce96a4b723864f2d57d59f15627891488cf04da2
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2025-08-16 03:01:25 -0700

    feat: install dependencies

---
+++ .lintstagedrc.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+module.exports = {
+  "supabase/*.{js,ts}": ["deno fmt"],
+  "*.{js,jsx,ts,tsx}": [
+    buildEslintCommand,
+    "prettier --write",
+    "tsc-files --noEmit",
+  ],
+};
