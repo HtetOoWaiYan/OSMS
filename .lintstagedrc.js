06f3e4e4ed402ec9ac635b0b10acc62d80ae78c4
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2025-08-18 09:29:21 -0700

    refactor: format code

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,15 +1,11 @@
-const path = require("path");
+const path = require('path');
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
+    .join(' --file ')}`;
 
 module.exports = {
-  "supabase/*.{js,ts}": ["deno fmt"],
-  "*.{js,jsx,ts,tsx}": [
-    buildEslintCommand,
-    "prettier --write",
-    "tsc-files --noEmit",
-  ],
+  'supabase/*.{js,ts}': ['deno fmt'],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write', 'tsc-files --noEmit'],
 };
