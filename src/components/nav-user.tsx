f3f6d346889362f645e64a221270fe60d02ea119
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2025-08-23 01:51:50 -0700

    feat: implement project settings management and update functionality

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { MoreHorizontal, LogOut, User, Settings } from 'lucide-react';
+import Link from 'next/link';
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
@@ -95,9 +96,11 @@ export function NavUser({
                 <User className="mr-2 h-4 w-4" />
                 Profile
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Settings className="mr-2 h-4 w-4" />
-                Settings
+              <DropdownMenuItem asChild>
+                <Link href="/dashboard/settings">
+                  <Settings className="mr-2 h-4 w-4" />
+                  Settings
+                </Link>
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
